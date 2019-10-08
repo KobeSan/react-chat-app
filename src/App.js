a78a0c248a7a24203a72963d37b57e2fa9eab04e
KobeSan <baptiste.gislot@gmail.com>
2019-10-08 09:46:57 +0200

    premier commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact name="Marshall Walker" avatar="https://randomuser.me/api/portraits/men/24.jpg" online={true} />
+      <Contact name="Tracy Perry" avatar="https://randomuser.me/api/portraits/men/49.jpg" online={false} />
+      <Contact name="Anita Baker" avatar="https://randomuser.me/api/portraits/women/33.jpg" online={true} />
+      <Contact name="Cathy Ramirez" avatar="https://randomuser.me/api/portraits/women/11.jpg" online={true} />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Contact.css';
+import { tsPropertySignature } from '@babel/types';
+
+function Contact(props) {
+  return (
+    <div className="Contact">
+      <img className="avatar" src={props.avatar} alt="avatar"></img>
+      <div>
+        <p className="name">
+          {props.name}
+        </p>
+        {props.online ?
+          <div className="status"><div className="status-online"></div><p className="status-text">online</p></div> :
+          <div className="status"><div className="status-offline"></div><p className="status-text">offline</p></div>}
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
