73f5457023f45dc69b67c5e6e94701b0085c10d4
KobeSan <baptiste.gislot@gmail.com>
2019-10-17 16:21:55 +0200

    Ajout du changement de status

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,30 @@ import React from 'react';
 
 import './Contact.css';
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} alt="avatar"></img>
-      <div>
-        <p className="name">
-          {props.name}
-        </p>
-        {props.online ?
-          <div className="status"><div className="status-online"></div><p className="status-text">online</p></div> :
-          <div className="status"><div className="status-offline"></div><p className="status-text">offline</p></div>}
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false
+    };
+  }
+  render() {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt="avatar"></img>
+        <div>
+          <p className="name">
+            {this.props.name}
+          </p>
+            <div onClick={() => {
+              this.setState = {
+                online: !this.state.online
+              };
+            }} className="status"><div className={this.state.online ? 'status-online' : 'status-offline'}></div><p className="status-text">{this.state.online ? 'online' : 'offline'}</p></div> 
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Contact;
